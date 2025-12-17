b65012c36c0ad02397a97735638b7453c05d54bb
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-17 09:19:19 +0530

    Add rate limiting to submission routes to enhance API security

--- src/routes/submission.route.js
+++ src/routes/submission.route.js
@@ -3,16 +3,19 @@ import verifyJWT from "../middlewares/auth.midleware.js"
 import authorizeRoles from "../middlewares/role.middleware.js";
 import { createSubmission, deleteSubmission, getAllSubmissions, getSingleSubmission, getSubmissionStatusForAssignment, gradingSubissions, mySubmissions, updateSubmission } from "../controllers/submission.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import rateLimiter from "../middlewares/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/create/:assignmentId", verifyJWT, authorizeRoles("student"), upload.array("files", 5), createSubmission)
+router.post("/create/:assignmentId", verifyJWT, authorizeRoles("student"), rateLimiter({ keyPrefix: "createSubmission", limit: 5, windowSec: 60 }), upload.array("files", 5), createSubmission)
+
 router.get("/submissions/:assignmentId", verifyJWT, authorizeRoles("admin", "teacher"), getAllSubmissions)
-router.post("/grade/:submissionId", verifyJWT, authorizeRoles("teacher", "admin"), gradingSubissions)
-router.patch("/delete/:submissionId", verifyJWT, authorizeRoles("admin", "student"), deleteSubmission)
+router.post("/grade/:submissionId", verifyJWT, authorizeRoles("teacher", "admin"), rateLimiter({ keyPrefix: "gradeSubmissions", limit: 40, windowSec: 60 }), gradingSubissions)
+
+router.patch("/delete/:submissionId", verifyJWT, authorizeRoles("admin", "student"), rateLimiter({ keyPrefix: "deleteSubmission", limit: 15, windowSec: 60 }), deleteSubmission)
 router.get("/my-submissions", verifyJWT, authorizeRoles("student"), mySubmissions)
 router.get("/submission/:submissionId", verifyJWT, authorizeRoles("admin", "teacher"), getSingleSubmission)
-router.patch("/update/:submissionId", verifyJWT, authorizeRoles("student"), upload.array("files", 5), updateSubmission)
-router.get("/submission-status/:assignmentId", verifyJWT, authorizeRoles("admin", "teacher"), getSubmissionStatusForAssignment)
+router.patch("/update/:submissionId", verifyJWT, authorizeRoles("student"), rateLimiter({ keyPrefix: "updateSubmission", limit: 5, windowSec: 60 }), upload.array("files", 5), updateSubmission)
+router.get("/submission-status/:assignmentId", verifyJWT, authorizeRoles("admin", "teacher"), rateLimiter({ keyPrefix: "submissionsStats", limit: 30, windowSec: 60 }), getSubmissionStatusForAssignment)
 
 export default router;
